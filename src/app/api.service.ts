a9fbc46fff4ebf8ada04e1c3d8f1f119ead2f024
jhk <qwertyjhk11@naver.com>
2024-09-15 11:32:31 +0900

    이전 커밋 참고

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpEventType } from '@angular/common/http';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map, scan } from 'rxjs/operators';
 
 @Injectable({
